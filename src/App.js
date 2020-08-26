6ef56920163dfe982a4c3efc862a57cef105d419
lihc15 <Nico0929>
2020-08-26 15:54:53 +0800

    文件目录格式化

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import css from './assets/css/App.css';
+import './assets/css/App.css';
 
 function App() {
   return (
@@ -8,11 +9,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello I'm Lee
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://baidu.com"
           target="_blank"
           rel="noopener noreferrer"
         >

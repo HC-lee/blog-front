3c022e8ae1d9eed6b42bc3d2653f41f1d39a519c
lihc15 <Nico0929>
2020-08-26 17:14:19 +0800

    新增antd库

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-// import css from './assets/css/App.css';
 import './assets/css/App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button type="primary">Button</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello I'm Lee

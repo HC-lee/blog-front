78ae3334bfd37d793a964f8dd1247c1a2dbbbf8d
lihc15 <Nico0929>
2020-08-27 15:30:05 +0800

    使用ant生成首页

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './assets/css/App.css';
-import { Button } from 'antd';
+import React, { Component } from 'react';
+import { HashRouter as Router, Route } from "react-router-dom";
+import { enquireScreen } from 'enquire-js';
+import Header from './pages/Home/Nav2';
+import Footer from './pages/Home/Footer1';
+import Home from './pages/Home';
+import Page from './pages/Introduce';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Button type="primary">Button</Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello I'm Lee
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://baidu.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  Nav20DataSource,
+  Footer10DataSource,
+} from './pages/Home/data.source.js';
+
+let isMobile;
+enquireScreen((b) => {
+  isMobile = b;
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile,
+    };
+  }
+  componentDidMount() {
+    // 适配手机屏幕;
+    enquireScreen((b) => {
+      this.setState({ isMobile: !!b });
+    });
+  }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header dataSource={Nav20DataSource} isMobile={this.state.isMobile} />
+          <Route exact path="/" component={Home} />
+          <Route path="/abc" component={Page} />
+          <Footer dataSource={Footer10DataSource} isMobile={this.state.isMobile} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;

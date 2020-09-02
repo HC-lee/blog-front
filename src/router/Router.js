9020a34ccf6d0cc5f4fb7c47c0dee511f51e5968
lihc15 <Nico0929>
2020-09-02 18:19:04 +0800

    白色头部提交

--- src/router/Router.js
+++ src/router/Router.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import { HashRouter as Router, Route } from "react-router-dom";
 import { enquireScreen } from 'enquire-js';
 import Header from '../components/header/Header';
+// import Header3 from '../components/header/Header3';
 import Home from '../pages/Home';
 import Blog from '../pages/Blog';
 import Kit from '../pages/Kit';
 import About from '../pages/About';
-import NoMatch from '../pages/NoMatch';
 
 import {
     Nav20DataSource,
+    Nav30DataSource
 } from '../data/data.source.js';
 
 let isMobile;
